026f4d2a4cb0276b9c385b23e1c1f0ad19c79834
sabatms <saba.tms15@gmail.com>
2023-04-24 12:39:53 -0700

    add function.js file

---
+++ function/function.js
@@ -0,0 +1,59 @@
+function A(a1,a2,a3){
+    return (a1*a2) + a3;
+}
+A(1,4,5);
+console.log(A(1,4,5));
+//
+
+function B(b1,b2,b3){
+          b1+b2+b3;
+}
+B(1,2,3);
+console.log(B(1,2,3));
+//
+
+function myFunction1(p1,p2){
+    return p1*p2
+}
+myFunction1(3,4);
+console.log(myFunction1(3,4));
+//
+
+let x = C(7,8);
+function C(c,d){
+          return c*d
+}
+console.log(C(7,8));
+//
+
+var y = myFunction(4, 3);
+
+function myFunction(e, f) {
+  return e * f;
+}
+console.log(myFunction);
+//
+
+function toCelsius(v){
+          return (5/9) *(v-23);
+}
+toCelsius(v = 60);
+console.log(toCelsius(v=60));
+//
+
+function saba(parameter1,parameter2,parameter3){
+ return (parameter1*parameter2+parameter3) *(20/2);
+}
+saba(5,2,1);
+console.log(saba(5,2,1));
+//
+
+let m = Age(30)
+let s = "you are so beaitiful elahe i think you  are " +m+"years old!";
+function Age(Number){
+          return (Number-4);
+}
+Age(30);
+console.log(Age(30));
+
+//
