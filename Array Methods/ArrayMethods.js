1569f7a8f39703d822e8ff6685a3f7cc18c2cff1
sabatms <saba.tms15@gmail.com>
2023-04-27 00:16:46 -0700

    add change ArrayMethods.js file

--- Array Methods/ArrayMethods.js
+++ Array Methods/ArrayMethods.js
@@ -1,11 +1,15 @@
 const arr1 =[20,18,16,14,12,10,13,15,17,19];
 const final =arr1.length-1;
+arr1[0]=15;
+console.log([arr1])
 
-for (let i = 0;i <= final; i++){
-          if(arr1[i] %2 ==0){
-                    console.log(arr1[i]);
+arr1.forEach(test);
+function test(num){
+          if (num % 2 == 0){
+                    console.log(num);
           }
 }
+
 
 console.log(arr1);
 console.log(typeof arr1);
@@ -19,3 +23,5 @@ test:()=>10+16,
 }
 console.log(obj.test());
 console.log(obj.age);
+
+console.log(Array.isArray(arr1));
