3038e3eba42bb91937df8507913af7b20ece7a7c
sabatms <saba.tms15@gmail.com>
2023-05-06 22:04:52 -0700

    add JsFilter.js file

--- Jsfilter/JsFilter.js
+++ Jsfilter/JsFilter.js
@@ -1,5 +1,13 @@
 const arrays = [8,18,25,32,45,77,100];
 
-const result = arrays.filter((number) => number > 20 && number < 100 );
+const result = arrays.filter((number) => number > 10 );
+const result1= arrays.filter((number) => number < 100);
+const result2= arrays.filter((number) => number >20 && number< 100);
 
 console.log(result);
+console.log(result1);
+console.log(result2);
+
+const citys= ["new yok", "shiraz","London","sangapour","sanandaj"];
+const result3 = citys.filter(citys => citys.startsWith("s"));
+console.log(result3);
