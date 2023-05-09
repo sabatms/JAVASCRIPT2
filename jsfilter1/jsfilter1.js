947969d771130ae82b398ef765f8aaf33e3b212e
sabatms <saba.tms15@gmail.com>
2023-05-08 23:23:05 -0700

    change jsfilter1.js

--- jsfilter1/jsfilter1.js
+++ jsfilter1/jsfilter1.js
@@ -12,7 +12,7 @@ const numbers1=[1,3,5,7,9];
 console.log(numbers1);
 //
 
-const numbers2 = [1,2,3,4]
+const numbers2 = [1,2,3,4,5,6]
 const item1 = numbers2.map(Two);
 function Two(value){
           return value * 3;
@@ -24,4 +24,23 @@ function Three( item){
           return item  / 10;
 }
 console.log(item2);
+
+const item3 = numbers2
+.filter(n => n>=0)
+.map(n =>({value:n}))
+.filter(obj => obj.value > 1)
+.map(obj => obj.value);
+
+console.log(item3);
 //
+
+const text1 = "hello world";
+
+const newArr =text1.map(Four);
+function Four(Number){
+          return Number = 25;
+
+}
+const result1 = text1.concat("",newArr);
+
+console.log(result1);
