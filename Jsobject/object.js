7ebc8de4c4e7eb4d54cabc698d8f7fa5d9b7455d
Saba Tahmasebii Shirazii <110444941+sabatms@users.noreply.github.com>
2023-05-10 13:03:43 +0330

    object.js

---
+++ Jsobject/object.js
@@ -0,0 +1,106 @@
+const Fruit = {
+          name:"apple",
+          colors:"red, green , yellow",
+          size: "medium",
+          colories: 2000,
+}
+console.log(Fruit.name);
+console.log(Fruit.colors);
+console.log(Fruit.size);
+console.log(Fruit.colories);
+console.log(Fruit);
+
+const car ={
+          name:"BMW",
+          colors:"black, white",
+          size:"Long-chassis, Short-chassis",
+          country:"germany",
+          carHp:3000,
+}
+console.log(car.name);
+console.log(car.colors);
+console.log(car.size);
+console.log(car.country);
+console.log(car.carHp);
+console.log(car);
+
+const address ={
+          name:"saba",
+          country:"Iran",
+          city:"Shiraz",
+          street:"satarkhan",
+}
+console.log(address.name);
+console.log(address.country);
+console.log(address.city)
+console.log(address.street);
+console.log(address);
+
+const arrayFruits = [
+          {
+                    name: "bnana",
+                    color: "yellow",
+                    country:"Africa",
+                    colories:1000,
+          },
+          {
+                    name:"fig",
+                    color:"dark perople",
+                    country:"iran, shiraz",
+                    colories: 37,
+          },
+          {
+                    name:"pomegranate",
+                    color: "red",
+                    country:"Afghanestan",
+                    colories:100,
+          }
+]
+
+console.log(arrayFruits[0].name);
+console.log(arrayFruits[0].color);
+console.log(arrayFruits[0].country);
+console.log(arrayFruits[0].colories);
+console.log(arrayFruits[1].name);
+console.log(arrayFruits[1].color);
+console.log(arrayFruits[1].country);
+console.log(arrayFruits[1].colories);
+console.log(arrayFruits[2].name);
+console.log(arrayFruits[2].color);
+console.log(arrayFruits[2].country);
+console.log(arrayFruits[2].colories);
+console.log(arrayFruits);
+
+const arraycars =[
+          {
+                    name:"Benz",
+                    country:"germany",
+                    carhp:2000,
+                    color:"Black",
+          },
+          {
+                    name:"range rover",
+                    country:"Uk",
+                    carhp: 395,
+                    color:"white",
+          },
+          {
+                    name:"porsh",
+                    country:"italia",
+                    carhp:600,
+                    color:"blue",
+          },
+]
+console.log(arraycars[0].name);
+console.log(arraycars[0].country);
+console.log(arraycars[0].carhp);
+console.log(arraycars[0].color);
+console.log(arraycars[1].name);
+console.log(arraycars[1].country);
+console.log(arraycars[1].carhp);
+console.log(arraycars[1].color);
+console.log(arraycars[2].name);
+console.log(arraycars[2].country);
+console.log(arraycars[2].carhp);
+console.log(arraycars[2].color);
+
